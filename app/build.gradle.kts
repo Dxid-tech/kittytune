caea1e833ef7d2bfe2f50633d333410bfd33b612
alan7383 <alan.mussot@outlook.fr>
2026-09-20 14:16:24 +0200

    chore(lint): ignore MissingTranslation in lint and IDE inspections

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,13 @@ extensions.configure<ApplicationExtension> {
     testOptions {
         unitTests.isReturnDefaultValues = true
     }
+
+    lint {
+        lintConfig = file("lint.xml")
+        disable += "MissingTranslation"
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
 }
 
 dependencies {
